2fefb3bdf75fbf8219342b734dd220f78d94c5e7
Xylems <Alex.Duarte92@gmail.com>
2026-02-28 10:19:13 -0500

    chore: set sage green as default theme colors

--- mealie/schema/admin/about.py
+++ mealie/schema/admin/about.py
@@ -27,20 +27,20 @@ class AppInfo(MealieModel):
 
 
 class AppTheme(MealieModel):
-    light_primary: str = "#E58325"
-    light_accent: str = "#007A99"
-    light_secondary: str = "#973542"
+    light_primary: str = "#558B2F"
+    light_accent: str = "#8BC34A"
+    light_secondary: str = "#4A7C59"
     light_success: str = "#43A047"
     light_info: str = "#1976D2"
-    light_warning: str = "#FF6D00"
+    light_warning: str = "#F57F17"
     light_error: str = "#EF5350"
 
-    dark_primary: str = "#E58325"
-    dark_accent: str = "#007A99"
-    dark_secondary: str = "#973542"
+    dark_primary: str = "#7CB342"
+    dark_accent: str = "#AED581"
+    dark_secondary: str = "#66BB6A"
     dark_success: str = "#43A047"
     dark_info: str = "#1976D2"
-    dark_warning: str = "#FF6D00"
+    dark_warning: str = "#F57F17"
     dark_error: str = "#EF5350"
 
 
